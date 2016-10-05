81ebbe411bbcb982feff905be54fdbc395f02bcb
Rémi Lafage <remi.lafage@onera.fr>
2016-10-05 22:35:04 +0200

    linting

--- build/xdsm.js
+++ build/xdsm.js
@@ -9607,30 +9607,30 @@ function Graph(mdo) {
     this.edges.push(new Edge(item.from, item.to, item.name, idA, idB, isMulti));
   }, this);
 
-  mdo.chains.forEach(function(chain, i) {
+  mdo.chains.forEach(function(chain) {
     var echain = Graph.expand(chain);
-    echain.forEach(function(leaf_chain, k) {
-      if (leaf_chain.length < 2) {
-        throw new Error("Bad process chain (" + leaf_chain.length + "elt)");
+    echain.forEach(function(leafChain) {
+      if (leafChain.length < 2) {
+        throw new Error("Bad process chain (" + leafChain.length + "elt)");
       } else {
         this.chains.push([]);
         var ids = this.nodes.map(function(elt) {
           return elt.id;
         });
-        leaf_chain.forEach(function(item, j) {
+        leafChain.forEach(function(item, j) {
           if (j !== 0) {
-            var idA = ids.indexOf(leaf_chain[j - 1]);
+            var idA = ids.indexOf(leafChain[j - 1]);
             if (idA < 0) {
               throw new Error("Process chain element (" +
-                              leaf_chain[j - 1] + ") not found");
+                              leafChain[j - 1] + ") not found");
             }
-            var idB = ids.indexOf(leaf_chain[j]);
+            var idB = ids.indexOf(leafChain[j]);
             if (idB < 0) {
               throw new Error("Process chain element (" +
-                              leaf_chain[j] + ") not found");
+                              leafChain[j] + ") not found");
             }
             if (idA !== idB) {
-              this.chains[this.chains.length-1].push([idA, idB]);
+              this.chains[this.chains.length - 1].push([idA, idB]);
             }
           }
         }, this);
@@ -9642,65 +9642,71 @@ function Graph(mdo) {
 function _expand(chain) {
   var ret = [];
   var prev;
-  var flag_parallel = false;
   chain.forEach(function(item) {
     if (item instanceof Array) {
       if (item[0].hasOwnProperty('parallel')) {
         if (prev) {
-          ret = ret.slice(0, ret.length-1).concat(item[0].parallel.map(function(elt) { return [prev].concat(_expand([elt]), prev); }));
+          ret = ret.slice(0, ret.length - 1).concat(item[0].parallel.map(
+              function(elt) {
+                return [prev].concat(_expand([elt]), prev);
+              }));
         } else {
-          throw new Error("Bad chain structure : cannot parallel loop without previous starting point.");
-        }        
-      } else {
-        if (prev) {
-          ret = ret.concat(_expand(item), prev)
-        } else {
-          ret = ret.concat(_expand(item));
+          throw new Error("Bad chain structure : " +
+              "cannot parallel loop without previous starting point.");
         }
-      }
-      prev = ret[ret.length-1];
-    } else if (item.hasOwnProperty('parallel')) {
-      flag_parallel = true;
-      if (prev) {
-        ret = ret.slice(0, ret.length-1).concat(item.parallel.map(function(elt) { return [prev].concat(_expand([elt])); }));
+      } else if (prev) {
+        ret = ret.concat(_expand(item), prev);
       } else {
-        ret = ret.concat(item.parallel.map(function(elt) { return _expand([elt]); }));
-      }      
+        ret = ret.concat(_expand(item));
+      }
+      prev = ret[ret.length - 1];
+    } else if (item.hasOwnProperty('parallel')) {
+      if (prev) {
+        ret = ret.slice(0, ret.length - 1).concat(
+            item.parallel.map(function(elt) {
+              return [prev].concat(_expand([elt]));
+            }));
+      } else {
+        ret = ret.concat(item.parallel.map(
+            function(elt) {
+              return _expand([elt]);
+            }));
+      }
       prev = undefined;
     } else {
-      var i = ret.length-1;
-      var flag_parallel = false;
-      while (i>=0 && (ret[i] instanceof Array)) {
+      var i = ret.length - 1;
+      var flagParallel = false;
+      while (i >= 0 && (ret[i] instanceof Array)) {
         ret[i] = ret[i].concat(item);
-        i=i-1;
-        var flag_parallel = true;
+        i -= 1;
+        flagParallel = true;
       }
-      if (!flag_parallel) {
+      if (!flagParallel) {
         ret.push(item);
       }
       prev = item;
     }
   }, this);
   return ret;
-};
+}
 
 Graph.expand = function(item) {
   var expanded = _expand(item);
-  var result=[];
-  var current=[];
+  var result = [];
+  var current = [];
   expanded.forEach(function(elt) {
     if (elt instanceof Array) {
       if (current.length > 0) {
         current.push(elt[0]);
         result.push(current);
-        current=[];
+        current = [];
       }
       result.push(elt);
     } else {
       if (result.length > 0 && current.length === 0) {
-        var last_chain = result[result.length-1];
-        var last_elt = last_chain[last_chain.length-1];
-        current.push(last_elt);
+        var lastChain = result[result.length - 1];
+        var lastElt = lastChain[lastChain.length - 1];
+        current.push(lastElt);
       }
       current.push(elt);
     }
