4c38bfe14bf4845c6a950eac46fc7280c670322f
lafage <remi.lafage@irt-saintexupery.com>
2016-10-26 17:38:27 +0200

    more object-oriented

--- src/xdsm.js
+++ src/xdsm.js
@@ -1,3 +1,4 @@
+var d3 = require('d3');
 var Labelizer = require('./labelizer.js');
 
 var WIDTH = 1000;
@@ -17,214 +18,129 @@ function Cell(x, y, width, height) {
 }
 
 function Xdsm(graph, tooltip) {
-  
-  var svg = d3.select(".xdsm").append("svg")
-              .attr("width", WIDTH)
-              .attr("height", HEIGHT);
-  var grid = [];
+  this.graph = graph;
+  this.tooltip = tooltip;
+  this.svg = d3.select(".xdsm").append("svg").attr("width", WIDTH).attr("height", HEIGHT);
+  this.grid = [];
+};
 
-  if (graph.refname) {
-    var ref = svg.append("text").text(graph.refname);
+Xdsm.prototype.draw = function() {
+  var self = this;
+
+  if (self.graph.refname) {
+    var ref = self.svg.append("text").text(self.graph.refname);
     var bbox = ref[0][0].getBBox();
-    ref.attr("x", X_ORIG)
-         .attr("y", Y_ORIG+bbox.height)
-         .classed("title", true); 
+    ref.attr("x", X_ORIG).attr("y", Y_ORIG + bbox.height).classed("title", true);
   }
 
-  // kind: node || edge
-  function createTextGroup(kind) {
-    var textGroups = svg.selectAll("." + kind)
-            .data(graph[kind + "s"])
-          .enter().append("g")
-          .attr("class", function(d) {
-            var klass = kind === "node" ? d.type : "dataInter";
-            if (klass === "dataInter" && d.isIO()) {
-              klass = "dataIO";
-            }
-            return kind + " " + klass;
-          })
-          .each(function() {
-            var labelize = Labelizer.labelize().ellipsis(5);
-            d3.select(this).call(labelize);
-          });
-
-    d3.selectAll(".ellipsized").on("mouseover", function(d) {
-      tooltip.transition()
-        .duration(200)
-        .style("opacity", 0.9);
-      var tooltipize = Labelizer.tooltipize().text(d.name);
-      tooltip.call(tooltipize)
-        .style("width", "200px")
-        .style("left", (d3.event.pageX) + "px")
-        .style("top", (d3.event.pageY - 28) + "px");
-    })
-    .on("mouseout", function() {
-      tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
-    });
-
-    return textGroups;
-  }
-
-  var nodes = createTextGroup("node");
-  var edges = createTextGroup("edge");
+  var nodes = self._createTextGroup("node");
+  var edges = self._createTextGroup("edge");
 
   // Workflow
-  svg.selectAll(".workflow")
-    .data(graph.chains)
-  .enter().insert("g", ":first-child")
-    .attr("class", "workflow")
-    .selectAll("polyline")
-    .data(function(d) {
-      return d;
-    })
-  .enter().insert("polyline", ":first-child")
-    .attr("points", function(d) {
-      var i1 = d[0] < d[1] ? d[1] : d[0];
-      var i2 = d[0] < d[1] ? d[0] : d[1];
-      var w = CELL_W * (i1 - i2);
-      var h = CELL_H * (i1 - i2);
-      var points = [];
-      if (d[0] < d[1]) {
-        if (d[0] !== 0) {
-          points.push((-w) + ",0");
-        }
-        points.push("0,0");
-        if (d[1] !== 0) {
-          points.push("0," + h);
-        }
-      } else {
-        if (d[0] !== 0) {
-          points.push(w + ",0");
-        }
-        points.push("0,0");
-        if (d[1] !== 0) {
-          points.push("0," + (-h));
-        }
-      }
-      return points.join(" ");
-    })
-    .attr("transform", function(d) {
-      var i1 = d[0] < d[1] ? d[1] : d[0];
-      var i2 = d[0] < d[1] ? d[0] : d[1];
-      var w;
-      var h;
-      if (d[0] < d[1]) {
-        w = CELL_W * i1 + X_ORIG;
-        h = CELL_H * i2 + Y_ORIG;
-      } else {
-        w = CELL_W * i2 + X_ORIG;
-        h = CELL_H * i1 + Y_ORIG;
-      }
-      return "translate(" + (X_ORIG + w) + "," + (Y_ORIG + h) + ")";
-    });
+  self._createWorkflow();
 
-  function layoutText(items) {
-    items.each(function(d, i) {
-      var item = d3.select(this);
-      if (grid[i] === undefined) {
-        grid[i] = new Array(items.length);
-      }
-      item.select("text").each(function(d, j) {
-        var that = d3.select(this);
-        var data = item.data()[0];
-        var m = (data.row === undefined) ? i : data.row;
-        var n = (data.col === undefined) ? i : data.col;
-        var bbox = that[0][j].getBBox();
-        grid[m][n] = new Cell(-bbox.width / 2,
-                              0,
-                              bbox.width,
-                              bbox.height);
-        that.attr("x", function() {
-          return grid[m][n].x;
-        }).attr("y", function() {
-          return grid[m][n].y;
-        }).attr("width", function() {
-          return grid[m][n].width;
-        }).attr("height", function() {
-          return grid[m][n].height;
-        });
-      });
-    });
+  // Layout texts
+  self._layoutText(nodes);
+  self._layoutText(edges);
 
-    items.attr("transform", function(d, i) {
-      var m = (d.col === undefined) ? i : d.col;
-      var n = (d.row === undefined) ? i : d.row;
-      var w = CELL_W * m + X_ORIG;
-      var h = CELL_H * n + Y_ORIG;
-      return "translate(" + (X_ORIG + w) + "," + (Y_ORIG + h) + ")";
-    });
-  }
-
-  // layout text
-  layoutText(nodes);
-  layoutText(edges);
-
-  // rectangles for nodes
-  function customRect(node, d, i, offset) {
-    node.insert("rect", ":first-child").attr("x", function() {
-      return grid[i][i].x + offset - PADDING;
-    })
-    .attr("y", function() {
-      return -grid[i][i].height * 2 / 3 - PADDING - offset;
-    })
-    .attr("width", function() {
-      return grid[i][i].width + (PADDING * 2);
-    })
-    .attr("height", function() {
-      return grid[i][i].height + (PADDING * 2);
-    })
-    .attr("rx", function() {
-      var rounded = d.type === 'optimization' ||
-                    d.type === 'mda' || d.type === 'doe';
-      return rounded ? (grid[i][i].height + (PADDING * 2)) / 2 : 0;
-    })
-    .attr("ry", function() {
-      var rounded = d.type === 'optimization' || d.type === 'mda' ||
-                    d.type === 'doe';
-      return rounded ? (grid[i][i].height + (PADDING * 2)) / 2 : 0;
-    });
-  }
-
+  // Rectangles for nodes
   nodes.each(function(d, i) {
     var that = d3.select(this);
-    that.call(customRect, d, i, 0);
+    that.call(self._customRect.bind(self), d, i, 0);
     if (d.isMulti) {
-      that.call(customRect, d, i, 1 * Number(MULTI_OFFSET));
-      that.call(customRect, d, i, 2 * Number(MULTI_OFFSET));
+      that.call(self._customRect.bind(self), d, i, 1 * Number(MULTI_OFFSET));
+      that.call(self._customRect.bind(self), d, i, 2 * Number(MULTI_OFFSET));
     }
   });
 
-  // trapezium for edges
-  function customTrapz(edge, d, i, offset) {
-    edge.insert("polygon", ":first-child").attr("points", function(d) {
-      var pad = 5;
-      var w = grid[d.row][d.col].width;
-      var h = grid[d.row][d.col].height;
-      var topleft = (-pad - w / 2 + offset) + ", " +
-                    (-pad - h * 2 / 3 - offset);
-      var topright = (w / 2 + pad + offset + 5) + ", " +
-                     (-pad - h * 2 / 3 - offset);
-      var botright = (w / 2 + pad + offset - 5 + 5) + ", " +
-                     (pad + h / 3 - offset);
-      var botleft = (-pad - w / 2 + offset - 5) + ", " + (pad + h / 3 - offset);
-      var tpz = [topleft, topright, botright, botleft].join(" ");
-      return tpz;
-    });
-  }
-
+  // Trapezium for edges
   edges.each(function(d, i) {
     var that = d3.select(this);
-    that.call(customTrapz, d, i, 0);
+    that.call(self._customTrapz.bind(self), d, i, 0);
     if (d.isMulti) {
-      that.call(customTrapz, d, i, 1 * Number(MULTI_OFFSET));
-      that.call(customTrapz, d, i, 2 * Number(MULTI_OFFSET));
+      that.call(self._customTrapz.bind(self), d, i, 1 * Number(MULTI_OFFSET));
+      that.call(self._customTrapz.bind(self), d, i, 2 * Number(MULTI_OFFSET));
     }
   });
 
   // Dataflow
-  var dataflow = svg.insert("g", ":first-child").attr("class", "dataflow");
+  self._createDataflow(edges);
+
+  // set svg size
+  var w = CELL_W * (self.graph.nodes.length + 1);
+  var h = CELL_H * (self.graph.nodes.length + 1);
+  self.svg.attr("width", w).attr("height", h);
+};
+
+Xdsm.prototype._createTextGroup = function(kind) {
+  var self = this;
+  var textGroups = self.svg.selectAll("." + kind).data(this.graph[kind + "s"]).enter().append("g").attr("class", function(d) {
+    var klass = kind === "node" ? d.type : "dataInter";
+    if (klass === "dataInter" && d.isIO()) {
+      klass = "dataIO";
+    }
+    return kind + " " + klass;
+  }).each(function() {
+    var labelize = Labelizer.labelize().ellipsis(5);
+    d3.select(this).call(labelize);
+  });
+
+  d3.selectAll(".ellipsized").on("mouseover", function(d) {
+    self.tooltip.transition().duration(200).style("opacity", 0.9);
+    var tooltipize = Labelizer.tooltipize().text(d.name);
+    self.tooltip.call(tooltipize).style("width", "200px").style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY - 28) + "px");
+  }).on("mouseout", function() {
+    self.tooltip.transition().duration(500).style("opacity", 0);
+  });
+
+  return textGroups;
+};
+
+Xdsm.prototype._createWorkflow = function() {
+  this.svg.selectAll(".workflow").data(this.graph.chains).enter().insert("g", ":first-child").attr("class", "workflow").selectAll("polyline").data(function(d) {
+    return d;
+  }).enter().insert("polyline", ":first-child").attr("points", function(d) {
+    var i1 = d[0] < d[1] ? d[1] : d[0];
+    var i2 = d[0] < d[1] ? d[0] : d[1];
+    var w = CELL_W * (i1 - i2);
+    var h = CELL_H * (i1 - i2);
+    var points = [];
+    if (d[0] < d[1]) {
+      if (d[0] !== 0) {
+        points.push((-w) + ",0");
+      }
+      points.push("0,0");
+      if (d[1] !== 0) {
+        points.push("0," + h);
+      }
+    } else {
+      if (d[0] !== 0) {
+        points.push(w + ",0");
+      }
+      points.push("0,0");
+      if (d[1] !== 0) {
+        points.push("0," + (-h));
+      }
+    }
+    return points.join(" ");
+  }).attr("transform", function(d) {
+    var i1 = d[0] < d[1] ? d[1] : d[0];
+    var i2 = d[0] < d[1] ? d[0] : d[1];
+    var w;
+    var h;
+    if (d[0] < d[1]) {
+      w = CELL_W * i1 + X_ORIG;
+      h = CELL_H * i2 + Y_ORIG;
+    } else {
+      w = CELL_W * i2 + X_ORIG;
+      h = CELL_H * i1 + Y_ORIG;
+    }
+    return "translate(" + (X_ORIG + w) + "," + (Y_ORIG + h) + ")";
+  });
+};
+
+Xdsm.prototype._createDataflow = function(edges) {
+  var dataflow = this.svg.insert("g", ":first-child").attr("class", "dataflow");
 
   edges.each(function(d, i) {
     dataflow.insert("polyline", ":first-child").attr("points", function() {
@@ -259,69 +175,75 @@ function Xdsm(graph, tooltip) {
       return "translate(" + (X_ORIG + w) + "," + (Y_ORIG + h) + ")";
     });
   });
+};
 
-  // set svg size
-  var w = CELL_W * (graph.nodes.length + 1);
-  var h = CELL_H * (graph.nodes.length + 1);
-  svg.attr("width", w).attr("height", h);
-
-  d3.select("button").on("click", function() {
-    computedStyleToInlineStyle(svg[0][0], true);
-
-    var html = d3.select("svg").attr("title", "xdsm")
-                  .attr("version", 1.1)
-                  .attr("xmlns", "http://www.w3.org/2000/svg")
-                  .node().parentNode.innerHTML;
-
-    var imgsrc = "data:image/svg+xml;base64," +
-                  btoa(unescape(encodeURIComponent(html)));
-    var canvas = d3.select("canvas").attr("width", w).attr("height", h);
-    var context = canvas[0][0].getContext("2d");
-
-    var image = new Image();
-    image.src = imgsrc;
-    image.onload = function() {
-      context.drawImage(image, 0, 0);
-      var canvasdata = canvas[0][0].toDataURL("image/png");
-      var pngimg = '<img src="' + canvasdata + '">';
-      d3.select("#pngdataurl").html(pngimg);
-      var a = document.createElement("a");
-      a.download = "xdsm.png";
-      a.href = canvasdata;
-      document.body.appendChild(a);
-      a.click();
-    };
+Xdsm.prototype._layoutText = function(items) {
+  var grid = this.grid;
+  items.each(function(d, i) {
+    var item = d3.select(this);
+    if (grid[i] === undefined) {
+      grid[i] = new Array(items.length);
+    }
+    item.select("text").each(function(d, j) {
+      var that = d3.select(this);
+      var data = item.data()[0];
+      var m = (data.row === undefined) ? i : data.row;
+      var n = (data.col === undefined) ? i : data.col;
+      var bbox = that[0][j].getBBox();
+      grid[m][n] = new Cell(-bbox.width / 2, 0, bbox.width, bbox.height);
+      that.attr("x", function() {
+        return grid[m][n].x;
+      }).attr("y", function() {
+        return grid[m][n].y;
+      }).attr("width", function() {
+        return grid[m][n].width;
+      }).attr("height", function() {
+        return grid[m][n].height;
+      });
+    });
   });
-}
 
-//this function adds inline style attribute to elements from css styles
-//It si required to generate proper image from d3 svg element.
-function computedStyleToInlineStyle(element, recursive) {
-  if (!element) {
-   throw new Error("No element specified.");
-  }
-  
-  if (!(element instanceof Element)) {
-   throw new Error("Specified element is not an instance of Element.");
-  }
-  
-  if (recursive) {
-   Array.prototype.forEach.call(element.children, function(child) {
-     computedStyleToInlineStyle(child, recursive);
-   });
-  }
-  
-  var computedStyle = getComputedStyle(element, null);
-  for (var i = 0; i < computedStyle.length; i++) {
-   var property = computedStyle.item(i);
-   // process only the properties used in the css
-   if (property === "fill" || property === "stroke" ||
-       property === "stroke-width" || property === "visibility") {
-     var value = computedStyle.getPropertyValue(property);
-     element.style[property] = value;
-   }
-}
-}
+  items.attr("transform", function(d, i) {
+    var m = (d.col === undefined) ? i : d.col;
+    var n = (d.row === undefined) ? i : d.row;
+    var w = CELL_W * m + X_ORIG;
+    var h = CELL_H * n + Y_ORIG;
+    return "translate(" + (X_ORIG + w) + "," + (Y_ORIG + h) + ")";
+  });
+};
 
+Xdsm.prototype._customRect = function(node, d, i, offset) {
+  var grid = this.grid;
+  node.insert("rect", ":first-child").attr("x", function() {
+    return grid[i][i].x + offset - PADDING;
+  }).attr("y", function() {
+    return -grid[i][i].height * 2 / 3 - PADDING - offset;
+  }).attr("width", function() {
+    return grid[i][i].width + (PADDING * 2);
+  }).attr("height", function() {
+    return grid[i][i].height + (PADDING * 2);
+  }).attr("rx", function() {
+    var rounded = d.type === 'optimization' || d.type === 'mda' || d.type === 'doe';
+    return rounded ? (grid[i][i].height + (PADDING * 2)) / 2 : 0;
+  }).attr("ry", function() {
+    var rounded = d.type === 'optimization' || d.type === 'mda' || d.type === 'doe';
+    return rounded ? (grid[i][i].height + (PADDING * 2)) / 2 : 0;
+  });
+};
+
+Xdsm.prototype._customTrapz = function(edge, d, i, offset) {
+  var grid = this.grid;
+  edge.insert("polygon", ":first-child").attr("points", function(d) {
+    var pad = 5;
+    var w = grid[d.row][d.col].width;
+    var h = grid[d.row][d.col].height;
+    var topleft = (-pad - w / 2 + offset) + ", " + (-pad - h * 2 / 3 - offset);
+    var topright = (w / 2 + pad + offset + 5) + ", " + (-pad - h * 2 / 3 - offset);
+    var botright = (w / 2 + pad + offset - 5 + 5) + ", " + (pad + h / 3 - offset);
+    var botleft = (-pad - w / 2 + offset - 5) + ", " + (pad + h / 3 - offset);
+    var tpz = [topleft, topright, botright, botleft].join(" ");
+    return tpz;
+  });
+};
 
 module.exports = Xdsm;
--- xdsm-main.js
+++ xdsm-main.js
@@ -5,34 +5,33 @@
 "use strict";
 
 var d3 = require('d3');
-var Graph = require('./src/graph.js');
-var Xdsm = require('./src/xdsm.js');
+var Graph = require('./src/graph');
+var Xdsm = require('./src/xdsm');
 
 d3.json("xdsm.json", function(error, mdo) {
   if (error) {
     throw error;
   }
-  
+
   var tooltip = d3.select("body").selectAll(".tooltip").data(['tooltip'])
                   .enter().append("div")
                 .attr("class", "tooltip")
                 .style("opacity", 0);
-  
+
   var scenarioKeys = Object.keys(mdo).sort();
-  if (scenarioKeys.indexOf('root') !== -1) {
+  if (scenarioKeys.indexOf('root') === -1) {
+    // old format: mono xdsm
+    var graph = new Graph(mdo);
+    var xdsm = new Xdsm(graph);
+    xdsm.draw();
+  } else {
     // new format managing several XDSM
     scenarioKeys.forEach(function(k) {
       if (mdo.hasOwnProperty(k)) {
         var graph = new Graph(mdo[k], k);
-        var xdsm = Xdsm(graph, tooltip);
+        var xdsm = new Xdsm(graph, tooltip);
+        xdsm.draw();
       }
     }, this);
-  } else {
-    // old format: mono xdsm
-    var graph = new Graph(mdo);
-    var xdsm = new Xdsm(graph);
   }
 });
-
-
-
