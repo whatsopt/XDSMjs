fb0cbda0f1ff5546a4575cbcd15a23b6519e22f3
Rémi Lafage <remi.lafage@onera.fr>
2019-01-14 10:06:04 +0100

    linting

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    xdsm: './xdsm-main.js',
-    'xdsm-test': './test/xdsm-test.js'
+    'xdsm': './src/xdsm-factory.js',
+    'xdsm-test': './test/xdsm-test.js',
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
   },
   devtool: 'source-map',
   module: {
@@ -20,16 +20,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
   node: {
-    fs: 'empty'
+    fs: 'empty',
   },
   plugins: [
-    new UglifyJSPlugin({ sourceMap: true })
-  ]
+    new UglifyJSPlugin({sourceMap: true}),
+  ],
 };
