6e279d3617298969e205809ec1fcf217188670d7
Rémi Lafage <remi.lafage@onera.fr>
2019-12-01 19:17:17 +0100

    eslint airbnb config

--- src/xdsm-factory.js
+++ src/xdsm-factory.js
@@ -2,8 +2,8 @@
  * XDSMjs
  * Copyright 2016-2019 Rémi Lafage
  */
-import {json} from 'd3-fetch';
-import {select, selectAll, event} from 'd3-selection';
+import { json } from 'd3-fetch';
+import { select, selectAll, event } from 'd3-selection';
 import Graph from './graph';
 import Xdsm from './xdsm';
 import Animation from './animation';
@@ -14,7 +14,7 @@ class XdsmFactory {
     const mdostr = elt.attr('data-mdo');
     if (!mdostr) {
       const filename = elt.attr('data-mdo-file') || 'xdsm.json';
-      json(filename).then((mdo) => this._createXdsm(mdo) );
+      json(filename).then((mdo) => this._createXdsm(mdo));
     } else {
       const mdo = JSON.parse(mdostr);
       this._createXdsm(mdo);
@@ -33,49 +33,51 @@ class XdsmFactory {
     const scenarioKeys = Object.keys(mdo).sort();
 
     // Optimization problem display setup
-    select("body").selectAll("optpb").data(scenarioKeys).enter().append("div")
-        .filter(function(d) {
-          return mdo[d].optpb;
-        }).attr("class", function(d) {
-          return "optpb " + d;
-        }).style("opacity", 0).on("click", function() {
-          select(this).transition().duration(500) // eslint-disable-line
+    select('body').selectAll('optpb').data(scenarioKeys).enter()
+      .append('div')
+      .filter((d) => mdo[d].optpb)
+      .attr('class', (d) => `optpb ${d}`)
+      .style('opacity', 0)
+      .on('click', () => {
+        select(this).transition().duration(500) // eslint-disable-line
           // no-invalid-this
-              .style("opacity", 0).style("pointer-events", "none");
-        }).append("pre").html(function(d) {
-          return mdo[d].optpb;
-        });
+          .style('opacity', 0)
+          .style('pointer-events', 'none');
+      })
+      .append('pre')
+      .html((d) => mdo[d].optpb);
 
-    var xdsms = {};
+    const xdsms = {};
 
     if (scenarioKeys.indexOf('root') === -1) {
       // old format: mono xdsm
-      var graph = new Graph(mdo);
+      const graph = new Graph(mdo);
       xdsms.root = new Xdsm(graph, 'root', config);
       xdsms.root.draw();
     } else {
       // new format managing several XDSM
-      scenarioKeys.forEach(function(k) {
-        if (mdo.hasOwnProperty(k)) {
-          var graph = new Graph(mdo[k], k);
+      scenarioKeys.forEach((k) => {
+        if (Object.prototype.hasOwnProperty.call(mdo, k)) {
+          const graph = new Graph(mdo[k], k);
           xdsms[k] = new Xdsm(graph, k, config);
           xdsms[k].draw();
-          xdsms[k].svg.select(".optimization").on(
-              "click",
-              function() {
-                var info = select(".optpb." + k);
-                info.style("opacity", 0.9);
-                info.style("left", (event.pageX) + "px").style("top",
-                    (event.pageY - 28) + "px");
-                info.style("pointer-events", "auto");
-              });
+          xdsms[k].svg.select('.optimization').on(
+            'click',
+            () => {
+              const info = select(`.optpb.${k}`);
+              info.style('opacity', 0.9);
+              info.style('left', `${event.pageX}px`).style('top',
+                `${event.pageY - 28}px`);
+              info.style('pointer-events', 'auto');
+            },
+          );
         }
       }, this); // eslint-disable-line no-invalid-this
     }
 
-    var anim = new Animation(xdsms);
+    const anim = new Animation(xdsms);
     if (xdsms.root.hasWorkflow()) { // workflow is optional
-      var ctrls = new Controls(anim); // eslint-disable-line no-unused-vars
+      const ctrls = new Controls(anim); // eslint-disable-line no-unused-vars
     }
     anim.renderNodeStatuses();
   }
@@ -83,12 +85,11 @@ class XdsmFactory {
 
 const XDSM_FACTORY = new XdsmFactory();
 
-document.addEventListener("DOMContentLoaded",
-    (event) => {
-      const elts = selectAll(".xdsm");
-      elts.each(function(d, i) {
-        const elt = select(this); // eslint-disable-line no-invalid-this
-        XDSM_FACTORY.createXdsm(elt);
-      });
-    }
-);
+document.addEventListener('DOMContentLoaded',
+  (/* event */) => {
+    const elts = selectAll('.xdsm');
+    elts.each(function createXDSM(/* d, i */) {
+      const elt = select(this); // eslint-disable-line no-invalid-this
+      XDSM_FACTORY.createXdsm(elt);
+    });
+  });
